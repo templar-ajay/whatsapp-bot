ca3bd4512d1f46a787151a8675a792f73e6d17e3
templar-ajay <templar.ajay@gmail.com>
2023-07-21 01:01:09 +0530

    wip- ws

--- server.js
+++ server.js
@@ -4,22 +4,57 @@ const path = require("path");
 const fs = require("fs");
 const qrcode = require("qrcode-terminal");
 const express = require("express");
+const cors = require("cors");
 const app = express();
+const expressWs = require("express-ws")(app);
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(function (req, res, next) {
+  console.log("middleware");
+  req.testing = "testing";
+
+  return next();
+});
 
 app.get("/:clientId", (req, res) => {
-  const { clientId } = req.params;
-  createAndSaveClient(clientId)
-    .then((data) =>
-      res.send("client created and saved successfully. clientId: " + data)
-    )
-    .catch((err) => {
-      console.log(
-        "failed to create client, maybe the client is already created",
-        err
-      );
-    });
+  res.send("this api no longer words upgrade to ws");
+
+  //   const { clientId } = req.params;
+  //   createAndSaveClient(clientId)
+  //     .then((data) =>
+  //       res.send("client created and saved successfully. clientId: " + data)
+  //     )
+  //     .catch((err) => {
+  //       console.log(
+  //         "failed to create client, maybe the client is already created",
+  //         err
+  //       );
+  //     });
 });
+
+app.ws("/a", function (ws, req) {
+  console.log("ws request received", ws);
+  ws.on("message", function (msg) {
+    console.log("message received from client", msg);
+    const { command, clientId } = JSON.parse(msg);
+    if (command == "createClient") {
+      if (clientId?.length > 8) {
+        // create a new client
+        createAndSaveClient(clientId, ws)
+          .then((clientId) => {
+            console.log("clientID", clientId);
+            ws.send(
+              JSON.stringify({ state: "client-ready", clientId: clientId })
+            );
+          })
+          .catch((err) => JSON.stringify({ result: "error", error: err }));
+      }
+    }
+  });
+});
+
 app.post("/send-message", (req, res) => {
   const { clientId, phoneNumber, ...rest } = req?.body?.customData;
   const messages = extractMessages(rest);
@@ -38,6 +73,42 @@ app.post("/send-message", (req, res) => {
 app.listen(8080, () => {
   console.log("server connected to port 8080");
 });
+
+async function createAndSaveClient(clientId, ws) {
+  console.log("creating client with clientId", clientId);
+  return new Promise((clientReady, unableToCreateClient) => {
+    const client = new Client({
+      authStrategy: new LocalAuth({ clientId: clientId }),
+    });
+    client.on("qr", (qr) => {
+      // qrcode.generate(qr, { small: true });
+      ws.send(JSON.stringify({ state: "qr-received", qr: qr }));
+    });
+    client.on("ready", () => {
+      console.log("Client is ready!");
+      clientReady(clientId);
+      setTimeout(() => client.destroy(), 2000);
+    });
+    client.on("disconnected", () => {
+      console.log("client has disconnected");
+      setTimeout(() => {
+        fs.rm(
+          path.resolve(
+            __dirname,
+            `./.wwebjs_auth/session-${client.options.authStrategy.clientId}`
+          ),
+          { recursive: true },
+          (err) => {
+            console.log(err ? err : "file removed");
+          }
+        );
+      }, 500);
+    });
+
+    client.initialize();
+  });
+}
+
 async function createClientAndSendMessage({ clientId, phoneNumber, messages }) {
   return new Promise((messageSent, errorSendingMessage) => {
     const client = new Client({
@@ -59,39 +130,6 @@ async function createClientAndSendMessage({ clientId, phoneNumber, messages }) {
           console.log(`failed to send message, error:`, error);
           errorSendingMessage(error);
         });
-    });
-    client.on("disconnected", () => {
-      console.log("client has disconnected");
-      setTimeout(() => {
-        fs.rm(
-          path.resolve(
-            __dirname,
-            `./.wwebjs_auth/session-${client.options.authStrategy.clientId}`
-          ),
-          { recursive: true },
-          (err) => {
-            console.log(err ? err : "file removed");
-          }
-        );
-      }, 500);
-    });
-
-    client.initialize();
-  });
-}
-async function createAndSaveClient(clientId) {
-  console.log("creating client with clientId", clientId);
-  return new Promise((clientReady, unableToCreateClient) => {
-    const client = new Client({
-      authStrategy: new LocalAuth({ clientId: clientId }),
-    });
-    client.on("qr", (qr) => {
-      qrcode.generate(qr, { small: true });
-    });
-    client.on("ready", () => {
-      console.log("Client is ready!");
-      clientReady(clientId);
-      setTimeout(() => client.destroy(), 1000);
     });
     client.on("disconnected", () => {
       console.log("client has disconnected");
